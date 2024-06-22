d8a387cfb4644ec03ade440b10c510950d7b5300
Moe <zeussuez2019@gmail.com>
2024-06-22 19:32:25 +0300

    Displaying Critic Score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,9 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding={4}>
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
+          
         ))}
+
       </SimpleGrid>
     </>
   );
