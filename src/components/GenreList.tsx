3d2a17c842bcbd88cf52ef1e99f4ab7d3333fa86
Moe <zeussuez2019@gmail.com>
2024-06-23 18:36:22 +0300

    Rendering Skeletons while fetching Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
 import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const skeletons_2 = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+  ];
   const { data, isLoading, error } = useGenres();
   if (error) return null;
-  if (isLoading) return <Spinner />;
+  // if (isLoading) return <Spinner />;
   return (
     <>
       <List>
+        {isLoading &&
+          skeletons_2.map((skeleton) => (
+            <ListItem key={skeleton} paddingY="5px">
+              <GenreListSkeleton />
+            </ListItem>
+          ))}
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,14 @@
+import { HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  return (
+    <>
+      <HStack >
+        <Skeleton boxSize="32px" borderRadius={8}></Skeleton>
+        <SkeletonText height="30px" width={"70%"} />
+      </HStack>
+    </>
+  );
+};
+
+export default GenreListSkeleton;
