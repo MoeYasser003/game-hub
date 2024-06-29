ae723be1f75ff903727592d0c68635dafd0770de
Moe <zeussuez2019@gmail.com>
2024-06-29 19:18:27 +0300

    Ship genres and platforms with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,14 @@
 import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
+// **This calls the server for dynamic genres list
+// const useGenres = () => useData<Genre>("/genres");
 
-const useGenres = () => useData<Genre>("/genres");
-
+// ** This Statically renders the genre list
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,14 @@
 import useData from "./useData";
+import platforms from "../data/platforms";
 
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
+// ** This calls the server for dynamic platforms
+// const usePlatform = () => useData<Platform>("/platforms/lists/parents");
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
-
+//** This statically renders platforms
+const usePlatform = () => ({ data: platforms, isLoading: false, error: null });
 export default usePlatform;
